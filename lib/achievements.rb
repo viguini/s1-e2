0532d21124c8139322d7410088cfb06dac02ea5c
Felipe Doria <felipe.doria@gmail.com>
2010-09-13 15:13:20 -0300

    Overall Structure

--- lib/achievements.rb
+++ lib/achievements.rb
@@ -1,6 +1,10 @@
+# encoding: utf-8
+
 require 'rubygems'
 require "bundler/setup"
 
-module Achievements
-  
-end
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require "achievements/user.rb"
+require "achievements/repo.rb"
+require "achievements/achievement.rb"
---
+++ test/achievements/repo_test.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+require File.join(File.expand_path(File.dirname(__FILE__)), "..", "test_helper")
+
+class RepoTest < Test::Unit::TestCase
+  
+end
